7da1a2a0be2b1ecdc4900e2f77cee7943c77d3ae
piyush jain <piyushjain-pj@users.noreply.github.com>
2020-10-08 19:55:02 +0530

    new changes

--- js/counter.jquery.js
+++ js/counter.jquery.js
@@ -110,34 +110,3 @@ $('.count').each(function () {
 });
 
 
-
-
-
-////side Bar
-/// some script
-$(function () {
-    'use strict'
-    $("[data-trigger]").on("click", function () {
-        $(".textout").show();
-        var trigger_id = $(this).attr('data-trigger');
-        $(trigger_id).toggleClass("show");
-        $('body').toggleClass("offcanvas-active");
-    });
-
-    // close if press ESC button 
-    $(document).on('keydown', function (event) {
-        if (event.keyCode === 27) {
-            $(".navbar-collapse").removeClass("show");
-            $("body").removeClass("overlay-active");
-            $(".textout").hide();
-        }
-    });
-
-    // close button 
-    $(".btn-close").click(function (e) {
-        $(".navbar-collapse").removeClass("show");
-        $("body").removeClass("offcanvas-active");
-        $(".textout").hide();
-    });
-})
-
